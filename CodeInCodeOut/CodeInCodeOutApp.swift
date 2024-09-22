15e41ea55b4fa7ddf9a83ecd82bb151f49fd3788
Robin OBrien <robin.c.obrien@gmail.com>
2024-09-22 12:42:59 -0400

    Initial Commit

---
+++ CodeInCodeOut/CodeInCodeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeInCodeOutApp.swift
+//  CodeInCodeOut
+//
+//  Created by Robin O'Brien on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct CodeInCodeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
